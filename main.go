2e7a8e15d20a4db4772cc2cde230e58a93e49691
mathieul <mathieu.lienard@ynov.com>
2024-05-16 15:32:15 +0200

    jean neymar

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var prout int = 0
 
 const port = ":8080"
 
@@ -13,7 +19,17 @@ var index = template.Must(template.ParseFiles("index.html"))
 var login = template.Must(template.ParseFiles("login.html"))
 var register = template.Must(template.ParseFiles("register.html"))
 
+var db *sql.DB
+
 func main() {
+	db, err := sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	InitTables(db)
+
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/login", Login)
 	http.HandleFunc("/register", Register)
@@ -45,6 +61,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 type loginErrors struct {
+	Wrongemail    bool
 	WrongUsername bool
 	WrongPassword bool
 }
@@ -63,10 +80,10 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 		r.ParseForm()
 		UsernameInput = r.FormValue("userName")
-		fmt.Println("User name : ", CurrentSession.user.name)
+		fmt.Println("User name : ", UsernameInput)
 
 		PasswordInput = r.FormValue("userPassword")
-		fmt.Println("User password : ", CurrentSession.user.password)
+		fmt.Println("User password : ", PasswordInput)
 	}
 
 	time.Sleep(200 * time.Millisecond)
@@ -107,4 +124,104 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
+
+	var currentErrors = loginErrors{}
+
+	var EmailInput string
+	var UsernameInput string
+	var PasswordInput string
+
+	if r.Method == "POST" {
+		time.Sleep(69 * time.Millisecond)
+
+		r.ParseForm()
+
+		EmailInput = r.FormValue("newEmail")
+		fmt.Println("User email : ", EmailInput)
+
+		UsernameInput = r.FormValue("newUserName")
+		fmt.Println("User name : ", UsernameInput)
+
+		PasswordInput = r.FormValue("newUserPassword")
+		fmt.Println("User password : ", PasswordInput)
+	}
+
+	if !ValidEmail(EmailInput) {
+		currentErrors.Wrongemail = true
+		fmt.Println("wrong email or name taken")
+	} else {
+		Request := `INSERT INTO Register (EMAIL,USERNAME,PASSWORD) VALUES (?, ?, ?);`
+		fmt.Println("Send : ", Request)
+		Prout()
+		_, err := db.Exec(Request, EmailInput, UsernameInput, PasswordInput)
+		Prout()
+		if err != nil {
+			log.Fatal(err)
+		}
+		Prout()
+
+		PrintTable("Register")
+	}
+
+	register.ExecuteTemplate(w, "register.html", currentErrors)
+}
+
+func InitTables(db *sql.DB) {
+	Register := `
+	CREATE TABLE Register (
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		EMAIL          TEXT    NOT NULL UNIQUE,
+		USERNAME       TEXT    NOT NULL UNIQUE,
+		PASSWORD       TEXT    NOT NULL
+	);
+	`
+	db.Exec(Register)
+
+	Posts := `
+	CREATE TABLE Posts (
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		USERNAME          TEXT    NOT NULL UNIQUE,
+		CONTENT           TEXT    NOT NULL UNIQUE,
+		IMAGE             TEXT
+	);
+	`
+	db.Exec(Posts)
+}
+
+func PrintTable(tableName string) {
+	rows, err := db.Query("SELECT * FROM " + tableName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var titre string
+		var auteur string
+		var date string
+		err = rows.Scan(&id, &titre, &auteur, &date)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, titre, auteur, date)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ValidEmail(email string) bool {
+	for _, i := range email {
+		if i == '@' {
+			return true
+		}
+	}
+	return false
+}
+
+func Prout() {
+	fmt.Println("prout %d", prout)
 }
